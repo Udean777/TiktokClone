dc570145a4e7efc785c9d175d675517575dd8466
Udean777 <uaart123@gmail.com>
2024-10-29 19:31:27 +0700

    base setup

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,82 @@
+import { Tabs } from "expo-router";
+import { Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#000",
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          height: 60,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              size={30}
+              color="black"
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="friends"
+        options={{
+          title: "Friends",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "people" : "people-outline"}
+              size={30}
+              color="black"
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="add_post"
+        options={{
+          title: "Add Post",
+          tabBarIcon: () => (
+            <View className="absolute bottom-3">
+              <Ionicons name={"add-circle"} size={75} color="black" />
+            </View>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="inbox"
+        options={{
+          title: "Inbox",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "chatbox-ellipses" : "chatbox-ellipses-outline"}
+              size={30}
+              color="black"
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "person" : "person-outline"}
+              size={30}
+              color="black"
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View className='items-center justify-center flex-1'>
+      <Text className='text-red-500 text-2xl font-bold'>Home</Text>
+    </View>
+  );
+}
